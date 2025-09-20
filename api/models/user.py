d3b49a60329137283d8f9c9a2dedf21f653d61a6
Brendan Stennett <brendan.stennett@gmail.com>
2025-09-20 17:44:37 -0400

    added alembic migrations

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,7 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
-Base: DeclarativeMeta = declarative_base()
+Base: DeclarativeMeta = declarative_base()  # pyright: ignore[reportAny]
 
-class User(SQLAlchemyBaseUserTableUUID, Base):
+class User(SQLAlchemyBaseUserTableUUID, Base):  # pyright: ignore[reportUntypedBaseClass]
     pass
--- api/setup/database.py
+++ api/setup/database.py
@@ -1,7 +1,5 @@
 from collections.abc import AsyncGenerator
-from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from api.models.user import Base
 
 sqlite_file_name = "database.sqlite"
 sqlite_url = f"sqlite+aiosqlite:///{sqlite_file_name}"
@@ -14,6 +12,5 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
-        await conn.run_sync(Base.metadata.create_all)
+    async with engine.begin():
+        pass
